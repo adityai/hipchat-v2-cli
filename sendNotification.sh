1537a4eefd3e370f7b2befca274e353490d8c4af
Aditya Inapurapu <adityaii@gmail.com>
2017-07-22 23:27:27 -0700

    Add env variable for room id

--- sendNotification.sh
+++ sendNotification.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -sS -H 'Content-type: application/json' -H "Authorization: Bearer $HIPCHAT_TOKEN" -d @message.json https://api.hipchat.com/v2/room/3076684/notification
+curl -sS -H 'Content-type: application/json' -H "Authorization: Bearer $HIPCHAT_TOKEN" -d @message.json https://api.hipchat.com/v2/room/$HIPCHAT_ROOM_ID/notification
